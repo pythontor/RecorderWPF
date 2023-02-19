bbd7be5a873babbdb084fa2c255619726e4541ae
pythontor <80468772+pythontor@users.noreply.github.com>
2023-02-19 16:22:06 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        string date_global;
+        List<jsons> jsons = new List<jsons>();
+        jsons json = new jsons();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+ private void Create_b_Click(object sender, RoutedEventArgs e)
+        {
+            json.rec_name=Rec_name_box.Text;
+            json.rec_desc=Rec_desc_box.Text;
+            json.rec_date=date_global;
+            jsons.Add(json);
+            Rec_desc_listbox.ItemsSource = jsons;
+            Rec_desc_listbox.DisplayMemberPath = "name";
+            Rec_name_box.Text = null;
+            Rec_desc_box.Text = null;
+            json.Serialize(jsons);
+        }
+
+        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DateTime date = Convert.ToDateTime(calendar.Text);
+            date_global = date.ToShortDateString();
+        }      
+
+        private void Delete_b_Click(object sender, RoutedEventArgs e)
+        {
+            Rec_desc_listbox.ItemsSource = null;
+            Rec_desc_listbox.DisplayMemberPath = null;
+            jsons.Clear();
+            string json = JsonConvert.SerializeObject(jsons);
+            File.WriteAllText("C:\\Users\\миша\\Desktop\\Records.json", json);
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/json.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class jsons
+    {
+        public string rec_name { get; set; }
+        public string rec_desc { get; set; }
+        public string rec_date { get; set; }
+        public void Serialize(List<jsons> jsons)
+        {
+            string json = JsonConvert.SerializeObject(jsons);
+            File.AppendAllText("C:\\Users\\миша\\Desktop\\Records.json", json);
+            string text = File.ReadAllText("C:\\Users\\миша\\Desktop\\Records.json");
+        }
+    }
+}
